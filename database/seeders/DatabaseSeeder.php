be327036898ec3784bb44da6832624fdb3280943
wangwei <wangwei171@126.com>
2021-01-04 06:25:45 +0000

    查看用户列表

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Model::unguard();
         // \App\Models\User::factory(10)->create();
+        $this->call(UsersTableSeeder::class);
+        Model::reguard();
     }
 }
---
+++ resources/views/users/_user.blade.php
@@ -0,0 +1,7 @@
+	<div class="list-group-item">
+		<img src="{{$user->gravatar()}}" alt="{{$user->name}}" width="32" class="mr-3">
+		<a href="{{route('users.show',$user)}}">
+			{{$user->name}}
+		</a>
+	</div>
+	
