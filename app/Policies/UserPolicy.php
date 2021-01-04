58b0d2b23d755ce0cbfa587f581ef4c6f6e3e3de
wangwei <wangwei171@126.com>
2021-01-04 10:38:34 +0000

    管理员可删除用户

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -24,7 +24,7 @@ class UserPolicy
         return $currentUser->id === $user->id;
     }
 
-    public function destory(User $currentUser,User $user)
+    public function destroy(User $currentUser,User $user)
     {
         return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
