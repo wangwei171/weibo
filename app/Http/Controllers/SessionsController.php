f691343bdf68dd782770cfb1f8c89e36e37be0fd
wangwei <wangwei171@126.com>
2021-01-11 23:48:50 +0000

    访问限流

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -13,6 +13,8 @@ class SessionsController extends Controller
     	$this->middleware('guest',[
     		'only' => ['create']
     	]);
+
+        $this->middleware('throttle:10,10',['only' => ['store']]);
     }
 
     public function create()
