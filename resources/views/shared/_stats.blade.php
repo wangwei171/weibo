667af83d808edb2cb7ed96a0068d4587f0105c5b
wangwei <wangwei171@126.com>
2021-01-13 07:24:56 +0000

    关注和粉丝列表页面

--- resources/views/shared/_stats.blade.php
+++ resources/views/shared/_stats.blade.php
@@ -1,20 +1,20 @@
 {{--统计关注了多少人、有多少个粉丝、有多少篇微博。3个a标签--}}
-<a href="#">
+<a href="{{ route('users.followings',$user->id) }}">
 	<strong id="following" class="stat">
 		{{ count($user->followings) }}
 	</strong>
 	关注
 </a>
 
-<a href="#">
+<a href="{{ route('users.followers',$user->id) }}">
 	<strong id="followers" class="stat">
 		{{ count($user->followers) }}
 	</strong>
 	粉丝
 </a>
 
-<a href="#">
-	<strong>
+<a href="{{ route('users.show',$user->id) }}">
+	<strong id="statuses" class="stat">
 		{{ $user->statuses()->count() }}
 	</strong>
 	微博
