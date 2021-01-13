5c58c75254dd4d73ec87a2dde98d689b4c00fcb8
wangwei <wangwei171@126.com>
2021-01-13 00:32:08 +0000

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use App\Models\Status;
 
 class StatusesController extends Controller
 {
@@ -23,6 +24,14 @@ class StatusesController extends Controller
     	]);
 
     	session()->flash('success','发布成功！');
+    	return redirect()->back();
+    }
+
+    public function destroy(Status $status)
+    {
+    	$this->authorize('destroy',$status);
+    	$status->delete();
+    	session()->flash('success','删除成功');
     	return redirect()->back();
     }
 }
